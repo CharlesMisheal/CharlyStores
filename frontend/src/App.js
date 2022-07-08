e1a076afb2d3405bf614b5649e987790de8b419e
Charles Misheal <charlesmisheal@gmail.com>
2022-07-08 12:09:25 +0100

    Online ecommerce webstite setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Charles Misheal Stores</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
